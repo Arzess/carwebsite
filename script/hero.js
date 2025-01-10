0bc2a7c8c8c30fc236713fee553c685fa7e64175
Arsenii <121680111+Arzess@users.noreply.github.com>
2025-01-10 21:55:27 +0100

    commentsfix

--- script/hero.js
+++ script/hero.js
@@ -4,8 +4,8 @@ const backlight = document.querySelector(".hero .backlight");
 const heroForm = document.querySelector(".hero .consultation-form");
 const safeAreaBottom = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue("--safe-area-inset-bottom")) || 0;
 
-let layoutAdjusted = false; // Flag to track if layout has been adjusted already
-let carsAdjusted = false; // Flag to track if the cars have been adjusted
+let layoutAdjusted = false;
+let carsAdjusted = false; 
 
 const adjustMargin = (element, property, value) => {
   if (window.innerWidth <= 500) {
@@ -14,7 +14,7 @@ const adjustMargin = (element, property, value) => {
 };
 
 const resetMargins = () => {
-  // Reset to default when the screen is wider than 500px
+
   heading.style.marginBottom = 0;
   cars.parentElement.style.marginBottom = 0;
   backlight.style.marginBottom = "50px";
@@ -28,17 +28,17 @@ const setRealVH = () => {
 
 const adjustHeroMobile = () => {
   if (window.innerWidth <= 500) {
-    heading.style.marginBottom = `${cars.clientHeight - 30}px`; // Adjust heading container based on the cars height
+    heading.style.marginBottom = `${cars.clientHeight - 30}px`; 
   }
 };
 
 const adjustCarsHeroMobile = () => {
   const marginBottom = heroForm.clientHeight + safeAreaBottom + "px";
   if (window.innerWidth <= 500 && !carsAdjusted) {
-    // Set the cars margin only once when the screen is <= 500px
+
     cars.parentElement.style.marginBottom = marginBottom;
     backlight.style.marginBottom = marginBottom;
-    carsAdjusted = true; // Mark that cars have been adjusted
+    carsAdjusted = true; 
   }
 };
 
@@ -47,8 +47,8 @@ const adjustForm = () => {
 };
 
 const adjustLayout = () => {
-  if (window.innerWidth <= 500 && !layoutAdjusted) { // Only adjust if screen is <= 500px and not adjusted yet
-    layoutAdjusted = true; // Prevent further adjustments
+  if (window.innerWidth <= 500 && !layoutAdjusted) {
+    layoutAdjusted = true;
 
     requestAnimationFrame(() => {
       adjustHeroMobile();
@@ -57,17 +57,14 @@ const adjustLayout = () => {
       setRealVH();
     });
   } else if (window.innerWidth > 500) {
-    resetMargins(); // Reset margins when the screen is larger than 500px
-    layoutAdjusted = false; // Allow adjustments again when going back to mobile version
-    carsAdjusted = false; // Reset cars adjustment flag
+    resetMargins();
+    layoutAdjusted = false; 
+    carsAdjusted = false; 
   }
 };
 
-// Event listener for page load and first resize
 const onLoadAndResize = () => {
   adjustLayout();
 };
-
-// Trigger layout adjustments on initial load and resize
 window.addEventListener("load", onLoadAndResize);
 window.addEventListener("resize", onLoadAndResize);
