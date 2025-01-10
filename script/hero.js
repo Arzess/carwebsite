468fa46b23d25752e8a5beda152d2499452b0cde
Arsenii <121680111+Arzess@users.noreply.github.com>
2025-01-10 12:51:22 +0100

    ios2

--- script/hero.js
+++ script/hero.js
@@ -5,7 +5,7 @@ const backlight = document.querySelector(".hero .backlight")
 const heroForm = document.querySelector(".hero .consultation-form")
 const mobileNavBar = document.querySelector(".mobile-navbar")
 const navBarHeight = 120;
-
+const safeAreaBottom = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue("--safe-area-inset-bottom")) || 0;
 const adjustHeroMobile = () => {
     if (window.innerWidth <= 500){
         heading.style.marginBottom = cars.clientHeight-30 + "px"; 
@@ -16,8 +16,8 @@ const adjustHeroMobile = () => {
 }
 const adjustCarsHeroMobile = () => {
     if (window.innerWidth <= 500){
-        cars.parentElement.style.marginBottom = heroForm.clientHeight+navBarHeight+"px"
-        backlight.style.marginBottom = heroForm.clientHeight+navBarHeight+"px"
+        cars.parentElement.style.marginBottom = heroForm.clientHeight+safeAreaBottom+"px"
+        backlight.style.marginBottom = heroForm.clientHeight+safeAreaBottom+"px"
     }
     else{
         backlight.style.marginBottom = 50 + "px";
@@ -26,7 +26,7 @@ const adjustCarsHeroMobile = () => {
 }
 const adjustForm = () => {
     if (window.innerWidth <= 500){
-        heroForm.parentElement.style.marginBottom = navBarHeight + "px";
+        heroForm.parentElement.style.marginBottom = safeAreaBottom + "px";
     }
     else{
         heroForm.parentElement.style.marginBottom = 0;
