f960b25bd3833a75be47bb3236197bdca2cce46a
Arsenii <121680111+Arzess@users.noreply.github.com>
2025-01-10 21:25:11 +0100

    optimization

--- script/hero.js
+++ script/hero.js
@@ -1,55 +1,60 @@
 const heading = document.querySelector(".hero .headings-container");
-
-const cars = document.querySelector(".hero .cars img")
-const backlight = document.querySelector(".hero .backlight")
-const heroForm = document.querySelector(".hero .consultation-form")
+const cars = document.querySelector(".hero .cars img");
+const backlight = document.querySelector(".hero .backlight");
+const heroForm = document.querySelector(".hero .consultation-form");
 const safeAreaBottom = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue("--safe-area-inset-bottom")) || 0;
 
 let resizeTimeoutVH;
+let resizeTimeout;
+
+const adjustMargin = (element, property, value) => {
+  if (window.innerWidth <= 500) {
+    element.style[property] = value;
+  } else {
+    element.style[property] = 0;
+  }
+};
+
 const setRealVH = () => {
-    const vh = window.innerHeight * 0.01; // Calculate 1% of the viewport height
-    document.documentElement.style.setProperty('--real-vh', `${vh}px`);
- };
+  const vh = window.innerHeight * 0.01; // Calculate 1% of the viewport height
+  document.documentElement.style.setProperty('--real-vh', `${vh}px`);
+};
 
 
 const adjustHeroMobile = () => {
-    if (window.innerWidth <= 500){
-        heading.style.marginBottom = cars.clientHeight-30 + "px"; 
-    }
-    else{
-        heading.style.marginBottom = 0;
-    }
-}
-const adjustCarsHeroMobile = () => {
-    if (window.innerWidth <= 500){
-        cars.parentElement.style.marginBottom = heroForm.clientHeight+safeAreaBottom+"px"
-        backlight.style.marginBottom = heroForm.clientHeight+safeAreaBottom+"px"
-    }
-    else{
-        backlight.style.marginBottom = 50 + "px";
-        cars.parentElement.style.marginBottom = 0;
-    }
-}
-const adjustForm = () => {
-    if (window.innerWidth <= 500){
-        heroForm.parentElement.style.marginBottom = safeAreaBottom + "px";
-    }
-    else{
-        heroForm.parentElement.style.marginBottom = 0;
-    }
-}
-
-let resizeTimeout;
-const adjustLayout = () => {
-    clearTimeout(resizeTimeout);
-    resizeTimeout = setTimeout(() => {
-        requestAnimationFrame(() => {
-            adjustHeroMobile();
-            adjustCarsHeroMobile();
-            adjustForm();
-            setRealVH();
-        });
-    }, 100); 
+  if (window.innerWidth <= 500) {
+    heading.style.marginBottom = `${cars.clientHeight - 30}px`; 
+  } else {
+    heading.style.marginBottom = 0;
+  }
 };
+
+const adjustCarsHeroMobile = () => {
+  const marginBottom = heroForm.clientHeight + safeAreaBottom + "px";
+  if (window.innerWidth <= 500) {
+    cars.parentElement.style.marginBottom = marginBottom;
+    backlight.style.marginBottom = marginBottom;
+  } else {
+    cars.parentElement.style.marginBottom = 0;
+    backlight.style.marginBottom = "50px";
+  }
+};
+
+const adjustForm = () => {
+  adjustMargin(heroForm.parentElement, 'marginBottom', `${safeAreaBottom}px`);
+};
+
+const adjustLayout = () => {
+  clearTimeout(resizeTimeout);
+  resizeTimeout = setTimeout(() => {
+    requestAnimationFrame(() => {
+      adjustHeroMobile();
+      adjustCarsHeroMobile();
+      adjustForm();
+      setRealVH();
+    });
+  }, 100); 
+};
+
 window.addEventListener("load", adjustLayout);
 window.addEventListener("resize", adjustLayout);
